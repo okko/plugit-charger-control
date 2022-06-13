e7f098c037066c75ccd72278b745870d6b0ce66f
Oskari Okko Ojala <git@okko.fi>
2022-06-13 12:11:10 +0300

    Use serverless-local-schedule to calculate DST schedules

--- src/functions/start-charging/index.ts
+++ src/functions/start-charging/index.ts
@@ -8,32 +8,34 @@ export default {
   }],
   timeout: 300,
   events: [
-    {
-      'schedule': 'cron(0 19 * 1-2,11-12 ? *)',
-    },
-    {
-      'schedule': 'cron(0 19 1-26 3 ? *)',
-    },
-    {
-      'schedule': 'cron(0 18 27-31 3 ? *)',
-    },
-    {
-      'schedule': 'cron(0 18 * 4-9 ? *)',
-    },
-    {
-      'schedule': 'cron(0 18 1-29 10 ? *)',
-    },
-    {
-      'schedule': 'cron(0 19 30-31 10 ? *)',
-    },
+    // {
+    //   'schedule': 'cron(0 19 * 1-2,11-12 ? *)',
+    // },
+    // {
+    //   'schedule': 'cron(0 19 1-26 3 ? *)',
+    // },
+    // {
+    //   'schedule': 'cron(0 18 27-31 3 ? *)',
+    // },
+    // {
+    //   'schedule': 'cron(0 18 * 4-9 ? *)',
+    // },
+    // {
+    //   'schedule': 'cron(0 18 1-29 10 ? *)',
+    // },
+    // {
+    //   'schedule': 'cron(0 19 30-31 10 ? *)',
+    // },
+    //
     // {
     //   'schedule': 'cron(0 21 * * ? *)',
     // },
-    // {
-    //   'schedule': {
-    //     rate: ['cron(0 21 * * ? *)'],
-    //     timezone: 'Europe/Helsinki',
-    //   }
-    // }
+    //
+    {
+       'schedule': {
+         rate: ['cron(0 21 * * ? *)'],
+         timezone: 'Europe/Helsinki',
+       }
+    },
   ],
 }
